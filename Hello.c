2fbd6d152cf9c7ee03f203fe541f3d0b4b5df9ac
Raijin9545 <157714784+Raijin9545@users.noreply.github.com>
2024-01-25 19:40:47 +0530

    basic programm of c

---
+++ Hello.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+ int main (){
+
+int x;
+int y;
+int z;
+
+printf("Type the value for x : ");
+scanf("%d",&x);
+
+printf("Type the value for y : ");
+scanf("%d",&y);
+
+printf("Type the value for z : ");
+scanf("%d",&z);
+
+printf("The average is%d",(x+y+z)/3);
+return 0;
+
+ }
